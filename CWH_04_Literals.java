aa6a2413023a93cfb256b6b79bbdc86482e85552
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-24 04:46:05 +0530

    Add files via upload

---
+++ CWH_04_Literals.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class CWH_04_Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        String str = "Abhijeet";
+        System.out.println(str);
+
+    }
+}
