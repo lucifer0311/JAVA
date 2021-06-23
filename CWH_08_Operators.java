05900634f58378bf3fe94cfec2aeb7410a894e6a
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-24 04:47:39 +0530

    Add files via upload

---
+++ CWH_08_Operators.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class CWH_08_Operators {
+    public static void main(String[] args) {
+        int a = 4;
+    //    int b = 6 % a;  // Modulo Operator
+        int b = 9;
+        b += 3;
+        System.out.println(b);
+        System.out.println(6==8); // Comparison Operator
+        System.out.println(64>8 && 64>9);  // Logical Operator (AND)
+        System.out.println(64>8 || 64>8);  // Logical Operator (OR)
+
+    }
+}
