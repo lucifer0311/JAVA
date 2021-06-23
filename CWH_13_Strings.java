d83904bd19e7447c6db175375725fabbaa3f0bb6
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-24 04:48:21 +0530

    Add files via upload

---
+++ CWH_13_Strings.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class CWH_13_Strings {
+    public static void main(String[] args) {
+        String name = "Abhijeet";
+    //  System.out.println("The name is: " + name);
+        System.out.print("The name is: ");
+        System.out.println(name);
+
+        int a = 4;
+        float b = 5.4565f;
+        System.out.printf("The value of a is %d and value of b is %f" , a , b);
+    }
+}
