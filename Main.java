663c1b60198d2249b295a53574d05aba6218071e
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-24 04:48:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!!");
+        System.out.print("The sum of these numbers is: ");
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = 4;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
