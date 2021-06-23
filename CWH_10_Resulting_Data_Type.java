c600d296ef31b43c4e09d77926a7695ec03e85bd
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-24 04:48:07 +0530

    Add files via upload

---
+++ CWH_10_Resulting_Data_Type.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class CWH_10_Resulting_Data_Type {
+    public static void main(String[] args) {
+        byte x = 5;
+        int y = 6;
+        short z = 8;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b);
+
+        // Increment And Decrement Opeartors
+
+        int i = 56;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+
+        char ch = 'B';
+        System.out.println(--ch);  // Decrement
+
+    }
+}
