22497e6d00e7aa2428c6b44a82520a4d87532c5e
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-25 17:56:26 +0530

    Add files via upload

---
+++ CWH_16_Conditionals.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CWH_16_Conditionals {
+    public static void main(String[] args) {
+        int age = 6;
+        if (age>18){
+            System.out.println("Yes boy you can Drive!!");
+        }
+        else{
+            System.out.println("No boy you cannot Drive yet!!");
+        }
+    }
+}
