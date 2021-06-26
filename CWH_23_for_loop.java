3499331488dbdabf4e36ac545c632ab9edf9e712
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-26 16:15:47 +0530

    Add files via upload

---
+++ CWH_23_for_loop.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class CWH_23_for_loop {
+    public static void main(String[] args) {
+    //    for (int i = 1 ; i<=10 ; i++){
+    //        System.out.println(i);
+    //    }
+
+    //    int n = 5;
+    //    for (int i = 0 ; i<n ; i++){
+    //        System.out.println(2*i + 1);
+    //    }
+
+        for (int i = 5 ; i>0 ; i--){
+            System.out.println(i);
+        }
+
+    }
+}
