ec03a38725e688257944de338365a517a49d6bc3
ABHIJEET JAIN <66820627+lucifer0311@users.noreply.github.com>
2021-06-26 15:46:22 +0530

    Add files via upload

---
+++ CWH_22_Do_While.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class CWH_22_Do_While {
+    public static void main(String[] args) {
+ //     int a = 0;
+ //     while(a<5){
+  //        System.out.println(a);
+ //         a++;
+  //    }
+
+        int b = 10;
+        do {
+            System.out.println(b);
+            b++;
+        }
+        while(b<5);
+
+    }
+}
